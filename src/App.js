a2efeb0b36e3dab37fea62fe12d74a184332d7d1
Andrew Smith <ajs3993@gmail.com>
2020-02-01 12:59:42 -0600

    list onClick onChange

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+onChange(event){
+console.log(event.target.value)
+}
+
+  render(){
+  
+  const list = [
+    'Item1',
+    'Item2',
+    'Another item'
+  ]
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    {
+      list.map(item => {
+        return(
+        <div onClick={this.onClick}>{item}</div>
+        )
+      })
+    }
+
+<input onChange = {this.onChange}/>
+
     </div>
   );
 }
-
+}
 export default App;
