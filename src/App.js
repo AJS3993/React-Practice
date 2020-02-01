90edfb6cb158de287c3ae5e17afd619043763b97
Andrew Smith <ajs3993@gmail.com>
2020-02-01 14:02:24 -0600

    state and props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, {Component} from 'react';
+import MyComponent from './MyComponent';
 import './App.css';
 
 class App extends Component {
-  
-onChange(event){
-console.log(event.target.value)
-}
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      title: 'hi'
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({
+      title: 'aksdfj'
+    });
+  }
+  
   render(){
-  
-  const list = [
-    'Item1',
-    'Item2',
-    'Another item'
-  ]
-
-  return (
-
-    <div className="App">
+    return (
+      <div className="App">
+        <h1>{this.state.title}</h1>
       
-    {
-      list.map(item => {
-        return(
-        <div onClick={this.onClick}>{item}</div>
-        )
-      })
-    }
-
-<input onChange = {this.onChange}/>
-
+        <div onClick={this.onClick}>click here</div>
+      
+        <MyComponent
+        title='im passing this from app.js to MyComponent.js'
+        />
     </div>
-  );
+    );
+  }
 }
-}
+
 export default App;
